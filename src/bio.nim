42b1b1bb5019529f342611507ce33ee5b54ba2e8
Xabi Bello <xbello@gmail.com>
2020-04-20 19:37:10 +0200

    Added a SeqRecord to allow the Fasta writing.

--- src/bio.nim
+++ src/bio.nim
@@ -6,6 +6,10 @@ type
   Seq* = ref object of RootObj
     chain*, class*: string
 
+  SeqRecord* = ref object of RootObj
+    name*: string
+    record*: Seq
+
   Dna* = ref object of Seq
 
 proc initDna*(chain: string): Dna =
@@ -14,6 +18,24 @@ proc initDna*(chain: string): Dna =
 
 proc `==`*(a, b: Dna): bool =
   a.chain == b.chain
+
+proc write*(record: SeqRecord, fHandler: File, kind: string) =
+  #kind is a string as in "fasta", to support different formats
+  const wrapSize: int = 60
+  fHandler.write(">", record.name, "\n")
+  var currentLen: int
+  for i in 0 .. (record.record.chain.len div wrapSize):
+    currentLen = min(wrapSize, record.record.chain[(i * wrapSize) .. ^1].len) - 1
+    fHandler.write(
+      record.record.chain[(i * wrapSize) .. ((i * wrapSize) + currentLen)])
+    fHandler.write("\n")
+  fHandler.flushFile
+
+proc write*(record: SeqRecord, fName, kind: string) =
+  let fHandler: File = open(fName, fmWrite)
+  defer: fHandler.close()
+
+  write(record, fHandler, kind)
 
 proc complement*(dna: Dna): Dna =
   result = initDna("")
--- tests/test_seqs.nim
+++ tests/test_seqs.nim
@@ -1,3 +1,6 @@
+import os
+import posix_utils
+import strutils
 import unittest
 
 import bio
@@ -25,3 +28,39 @@ suite "Test Seq operation":
   #
   #test "DNA transcription":
   #  fail "TDB"
+  #
+suite "Test SeqRecord operation":
+  setup:
+    var dna = Seq(chain: "ACGTGGGGT", class: "DNA")
+    var dnaRecord = SeqRecord(record: dna, name: "Sample")
+    var tmpOutput: (string, File) = mkstemp("tests/file_")
+
+  teardown:
+    tmpOutput[1].close()
+    removeFile(tmpOutput[0])
+
+  test "Write records as a FASTA through filename":
+    dnaRecord.write(tmpOutput[0], "fasta")
+
+    check existsFile(tmpOutput[0])
+
+    let fastaFile = tmpOutput[0].readLines(2)
+
+    check fastaFile == @[">Sample", "ACGTGGGGT"]
+
+  test "Write records as a FASTA through handler":
+    dnaRecord.write(tmpOutput[1], "fasta")
+
+    let fastaFile = tmpOutput[0].readLines(2)
+
+    check fastaFile == @[">Sample", "ACGTGGGGT"]
+
+  test "Automatically wrap the long lines to 60 chars":
+    dna.chain = repeat(dna.chain, 8)
+    dnaRecord.write(tmpOutput[1], "fasta")
+
+    let fastaFile = tmpOutput[0].readLines(3)
+
+    check fastaFile.len == 3
+    check fastaFile[1].len == 60
+    check fastaFile[1] == dna.chain[0 .. 59]
